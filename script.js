4281bd1b25fa9b86501088a3c7872d12c1b4505f
traffico <89013529+traffico@users.noreply.github.com>
2023-05-03 16:38:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("#take-picture-section").url = 'er';
+//       });
+// })
+
+// document.getElementById('take-tour-btn').addEventListener('click', function(){
+//     document.getElementById('section-one').style.background = url('../app2.jpg')
+// })
+
